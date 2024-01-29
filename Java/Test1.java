a91f5f3ee84572319b2e453460ef540c128099f2
Vinutha <vinuthareddy47@gmail.com>
2024-01-29 11:16:54 +0530

    sync

---
+++ Java/Test1.java
@@ -0,0 +1,28 @@
+class Outer
+{
+    private int x=10;
+    class Inner
+    {
+        private int y=20;
+        void display()
+        {
+            System.out.println("x= "+x+"y= "+y);
+        }
+    }
+    void display()
+    {
+        Inner i=new Inner();
+        i.display();
+        System.out.println("y= "+i.y);
+    }
+}
+public class Test1 
+{
+    public static void main(String[] args) 
+    {
+        Outer o=new Outer();
+        Outer.Inner obj=o.new Inner();
+        o.display();
+        obj.display();
+    }
+}
